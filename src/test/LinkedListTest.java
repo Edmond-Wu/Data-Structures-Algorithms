2ddb326f5eafe59647a975075de325d7278ebf36
Edmond Wud <edmond.wu0895@gmail.com>
2017-07-14 11:40:29 -0400

    added some more tests to linkedlisttest

--- src/test/LinkedListTest.java
+++ src/test/LinkedListTest.java
@@ -7,7 +7,7 @@ import structures.LinkedList;
 
 public class LinkedListTest extends TestCase {
 
-	LinkedList<Integer> listOne;
+	LinkedList<Integer> listOne, listTwo;
 	
 	@Before
 	public void setUp() {
@@ -20,12 +20,37 @@ public class LinkedListTest extends TestCase {
 		assertNull(listOne.getTail());
 		assertEquals(listOne.getSize(), 0);
 		listOne.addToFront(5);
-		assertEquals(listOne.getHead(), (Integer)5);
-		assertEquals(listOne.getTail(), listOne.getHead());
+		assertEquals(listOne.getHead().getData(), (Integer)5);
+		assertEquals(listOne.getTail().getData(), listOne.getHead().getData());
 		assertEquals(listOne.getSize(), 1);
 		listOne.addToFront(10);
-		assertEquals(listOne.getHead(), (Integer)10);
-		assertEquals(listOne.getTail(), (Integer)5);
+		assertEquals(listOne.getHead().getData(), (Integer)10);
+		assertEquals(listOne.getTail().getData(), (Integer)5);
 		assertEquals(listOne.getSize(), 2);
+	}
+	
+	@Test
+	public void testAddToBack() {
+		listOne.addToBack(5);
+		assertEquals(listOne.getHead().getData(), (Integer)5);
+		assertEquals(listOne.getTail().getData(), listOne.getHead().getData());
+		assertEquals(listOne.getSize(), 1);
+		listOne.addToBack(10);
+		assertEquals(listOne.getHead().getData(), (Integer)5);
+		assertEquals(listOne.getTail().getData(), (Integer)10);
+		assertEquals(listOne.getSize(), 2);
+	}
+	
+	@Test
+	public void testReverse() {
+		listOne.addToBack(1);
+		listOne.reverse();
+		assertEquals(listOne.toString(), "1");
+		for (int i = 2; i <= 6; i++) {
+			listOne.addToBack(i);
+		}
+		assertEquals(listOne.toString(), "1,2,3,4,5,6");
+		listOne.reverse();
+		assertEquals(listOne.toString(), "6,5,4,3,2,1");
 	}
 }
