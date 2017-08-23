2f0d91103741ba88491280d5f3a7d692046e2e45
Edmond Wud <edmond.wu0895@gmail.com>
2017-08-23 16:09:43 -0400

    fixed some formatting

--- src/test/MergeSortTest.java
+++ src/test/MergeSortTest.java
@@ -10,29 +10,29 @@ import sort.MergeSort;
 public class MergeSortTest extends TestCase {
 	@Test
 	public void testRandomInput() {
-		int[] arr = {5, 24, 15, 16, 8, 4, 7, 2, 50, 10, 10};
+		int[] arr = { 5, 24, 15, 16, 8, 4, 7, 2, 50, 10, 10 };
 		MergeSort.sort(arr);
 		assertEquals("[2, 4, 5, 7, 8, 10, 10, 15, 16, 24, 50]", Arrays.toString(arr));
 	}
-	
-    @Test
-    public void testReverseInput() {
-        int[] arr = {22, 21, 19, 18, 15, 14, 9, 7, 5};
-        MergeSort.sort(arr);
-        assertEquals("[5, 7, 9, 14, 15, 18, 19, 21, 22]", Arrays.toString(arr));
-    }
 
-    @Test
-    public void testEmptyInput() {
-        int[] arr = {};
-        MergeSort.sort(arr);
-        assertEquals("[]", Arrays.toString(arr));
-    }
+	@Test
+	public void testReverseInput() {
+		int[] arr = { 22, 21, 19, 18, 15, 14, 9, 7, 5 };
+		MergeSort.sort(arr);
+		assertEquals("[5, 7, 9, 14, 15, 18, 19, 21, 22]", Arrays.toString(arr));
+	}
 
-    @Test
-    public void testAlreadySorted() {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        MergeSort.sort(arr);
-        assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", Arrays.toString(arr));
-    }
+	@Test
+	public void testEmptyInput() {
+		int[] arr = {};
+		MergeSort.sort(arr);
+		assertEquals("[]", Arrays.toString(arr));
+	}
+
+	@Test
+	public void testAlreadySorted() {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		MergeSort.sort(arr);
+		assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", Arrays.toString(arr));
+	}
 }
--- src/test/QuickSortTest.java
+++ src/test/QuickSortTest.java
@@ -13,29 +13,29 @@ import sort.QuickSort;
 public class QuickSortTest extends TestCase {
 	@Test
 	public void testRandomInput() {
-		int[] arr = {5, 24, 15, 16, 8, 4, 7, 2, 50, 10, 10};
+		int[] arr = { 5, 24, 15, 16, 8, 4, 7, 2, 50, 10, 10 };
 		QuickSort.sort(arr, 0, arr.length - 1);
 		assertEquals("[2, 4, 5, 7, 8, 10, 10, 15, 16, 24, 50]", Arrays.toString(arr));
 	}
-	
-    @Test
-    public void testReverseInput() {
-        int[] arr = {22, 21, 19, 18, 15, 14, 9, 7, 5};
-        QuickSort.sort(arr, 0, arr.length - 1);
-        assertEquals("[5, 7, 9, 14, 15, 18, 19, 21, 22]", Arrays.toString(arr));
-    }
 
-    @Test
-    public void testEmptyInput() {
-        int[] arr = {};
-        QuickSort.sort(arr, 0, arr.length - 1);
-        assertEquals("[]", Arrays.toString(arr));
-    }
+	@Test
+	public void testReverseInput() {
+		int[] arr = { 22, 21, 19, 18, 15, 14, 9, 7, 5 };
+		QuickSort.sort(arr, 0, arr.length - 1);
+		assertEquals("[5, 7, 9, 14, 15, 18, 19, 21, 22]", Arrays.toString(arr));
+	}
 
-    @Test
-    public void testAlreadySorted() {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        QuickSort.sort(arr, 0, arr.length - 1);
-        assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", Arrays.toString(arr));
-    }
+	@Test
+	public void testEmptyInput() {
+		int[] arr = {};
+		QuickSort.sort(arr, 0, arr.length - 1);
+		assertEquals("[]", Arrays.toString(arr));
+	}
+
+	@Test
+	public void testAlreadySorted() {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		QuickSort.sort(arr, 0, arr.length - 1);
+		assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", Arrays.toString(arr));
+	}
 }
