f0bb04cb4a40a46dada681919e8b2cf7cefd4646
edmond <edmond.wu0895@gmail.com>
2022-03-23 15:01:56 -0400

    removed wildcard import

--- src/leetcode/LongestStringChain.java
+++ src/leetcode/LongestStringChain.java
@@ -1,6 +1,10 @@
 package leetcode;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class LongestStringChain {
     /**
