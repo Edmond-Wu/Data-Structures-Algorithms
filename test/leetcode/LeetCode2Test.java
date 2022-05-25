061614a98efd8f9ce81536dfb166fbae2210cd55
edmond <edmond.wu0895@gmail.com>
2022-05-25 13:19:33 -0400

    longest increasing subsequence

--- test/leetcode/LeetCode2Test.java
+++ test/leetcode/LeetCode2Test.java
@@ -3,7 +3,6 @@ package leetcode;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -70,8 +69,12 @@ public class LeetCode2Test {
     }
 
     @Test
-    public void testFindAllRecipes() {
-        String[] recipes1 = {"bread"};
-        List<List<String>> ingredients1 = new ArrayList<>();
+    public void testLongestIncreasingSubsequence() {
+        int[] arr1 = {10,9,2,5,3,7,101,18};
+        Assert.assertEquals(4, LeetCode2.longestIncreasingSubsequence(arr1));
+        int[] arr2 = {0,1,0,3,2,3};
+        Assert.assertEquals(4, LeetCode2.longestIncreasingSubsequence(arr2));
+        int[] arr3 = {7,7,7,7,7,7,7};
+        Assert.assertEquals(1, LeetCode2.longestIncreasingSubsequence(arr3));
     }
 }
